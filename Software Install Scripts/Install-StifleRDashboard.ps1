fe4acc06b00294b725489847f6eaa8aee91fc641
Mike Terrill <50811005+materrill@users.noreply.github.com>
2025-07-23 15:26:53 -0700

    Update Install-StifleRDashboard.ps1

--- Software Install Scripts/Install-StifleRDashboard.ps1
+++ Software Install Scripts/Install-StifleRDashboard.ps1
@@ -39,4 +39,45 @@ start-process "msiexec.exe" -arg $arguments -Wait
 Import-Module WebAdministration
 New-WebVirtualDirectory -Site "Default Web Site" -Name "StifleRDashboard" -PhysicalPath 'C:\Program Files\2Pint Software\StifleR Dashboards\Dashboard Files'
 
+# Configure IIS for Authentication
+$partofdomain = (Get-CimInstance win32_computersystem).PartOfDomain
+$siteName = "Default Web Site/StifleRDashboard"
+
+Write-Host "Removing Anonymous Authentication from website."
+Set-WebConfigurationProperty -filter /system.webServer/security/authentication/AnonymousAuthentication -name enabled -value false -PSPath IIS:\ -location $siteName
+
+Write-Host "Configuring IIS for Windows Authentication."
+Set-WebConfigurationProperty -filter /system.webServer/security/authentication/WindowsAuthentication -name enabled -value True -PSPath IIS:\ -location $siteName
+
+Write-Host "Configuring Windows Auth Providers"
+#First we remove all providers, disable Kernel-mode authentication, then re-add single authentication provider.
+Get-WebConfigurationProperty -Filter system.webServer/security/authentication/WindowsAuthentication `
+ -Location $siteName -Name providers.Collection | Select-Object -ExpandProperty Value | ForEach-Object {Remove-WebConfigurationProperty -Filter system.webServer/security/authentication/WindowsAuthentication -Location $SiteName -Name providers.Collection -AtElement @{value=$_}}
+
+$winKernel = (Get-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -location $siteName -filter /system.WebServer/security/authentication/windowsAuthentication -name "useKernelMode").Value
+if ($winKernel -eq $True){
+    Write-Host "Disabling Kernel-mode authentication."
+    Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -location $siteName -filter /system.WebServer/security/authentication/windowsAuthentication -name "useKernelMode" -value false
+}
+
+# If StifleR is running on a domain joined server, auth provider is configured to Negotiate:Kerberos.
+if ($partofdomain -eq $true){
+    Write-Host "Server is domain joined. Configuring Negotiate:Kerberos as authentication provider."
+    Add-WebConfigurationProperty -Filter system.webServer/security/authentication/WindowsAuthentication `
+ -Location $siteName -Name providers.Collection -AtIndex 0 -Value "Negotiate:Kerberos"
+}
+# If StifleR is running on a non-domain joined server, auth provider is configured to NTLM
+if ($partofdomain -eq $false){
+    Write-Host "Server is not member of a domain. Configuring NTLM as authentication provider."
+    Add-WebConfigurationProperty -Filter system.webServer/security/authentication/WindowsAuthentication `
+    -Location $siteName -Name providers.Collection -AtIndex 0 -Value "NTLM"
+}
+
+# Accessing server locally with fqdn can cause authentication prompt loop on workgroup server
+if ($partofdomain -eq $false) {
+    Write-Host "Server is not member of a domain. Configuring BackConnectionHostNames."
+    $multiStringData = @("$fqdn")
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0" -Name "BackConnectionHostNames" -Value $multiStringData -Type MultiString
+}
+
 Write-Host "Script completed."
