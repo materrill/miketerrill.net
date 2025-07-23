1daabc1b0734a185f7a21ef50d3bad1cc5d76916
Mike Terrill <50811005+materrill@users.noreply.github.com>
2025-07-23 10:34:24 -0700

    Create Install-StifleRDashboard.ps1

---
+++ Software Install Scripts/Install-StifleRDashboard.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    PowerShell script to perform an unattended install of StifleR Dashboard 
+.DESCRIPTION
+    This script automates installing StifleR Dashboard and will determine the FQDN of the server and use
+    it for the dashboard configuration. Make sure 2PXE is installed first and a FQDN cert
+    has been generated and the IIS 443 bindings have been configured. It will also create the IIS virtual
+    directory for the dashboard.
+.NOTES
+    Author: Mike Terrill/2Pint Software
+    Date: July 23, 2025
+    Version: 25.07.23
+    Requires: Administrative privileges, 64-bit Windows
+#>
+
+# Set path to MSI file
+$msifile = "$PSScriptRoot\StifleR-Dashboard-x64.msi"
+
+# Ensure the script runs with elevated privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "This script requires administrative privileges. Please run PowerShell as Administrator."
+    exit 1
+}
+
+# This will use the connection specific suffix for the fqdn - useful when system is not domain joined
+$domain = [string](Get-DnsClient | Select-Object -ExpandProperty ConnectionSpecificSuffix)
+$fqdn = "$($env:COMPUTERNAME.Trim()).$($domain.Trim())"
+$STIFLERSERVER = "https://$($fqdn):1414"
+$STIFLERLOCSERVER = "https://$($fqdn):9000"
+
+$arguments = "/i $msifile $STIFLERSERVER $STIFLERLOCSERVER /qn /norestart /l*v C:\Windows\Temp\StifleRDashboardInstall.log"
+
+write-host "Using the following install commands: $arguments" #uncomment this line to see the command line
+
+# Install the StifleR Dashboard
+start-process "msiexec.exe" -arg $arguments -Wait
+
+# Create the StifleR Dashboard IIS Virtual Directory
+Import-Module WebAdministration
+New-WebVirtualDirectory -Site "Default Web Site" -Name "StifleRDashboard" -PhysicalPath 'C:\Program Files\2Pint Software\StifleR Dashboards\Dashboard Files'
+
+Write-Host "Script completed."
